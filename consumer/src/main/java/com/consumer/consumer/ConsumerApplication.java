9ec29c12ff82f828ecda9ad4bd09ac821b51c491
KritamSen <kritamkumar14@gmail.com>
2026-01-02 11:52:24 +0530

    check1

--- consumer/src/main/java/com/consumer/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/consumer/consumer/ConsumerApplication.java
@@ -2,7 +2,7 @@ package com.consumer.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+////////kaisa hai be
 @SpringBootApplication
 public class ConsumerApplication {
 
